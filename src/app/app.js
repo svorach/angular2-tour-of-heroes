cfeb663dc25aafb70abdb0d3a0c5ef939407a544
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-10-26 14:41:33 -0400

    End of tutorial chapter 1.

--- src/app/app.js
+++ src/app/app.js
@@ -10,13 +10,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'app',
-            template: '<h1>Dub Tee Ef Angular 2 App</h1>'
+            directives: [angular2_1.FORM_DIRECTIVES],
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<div><label>id: </label>{{hero.id}}</div>\n\t\t<div>\n\t\t\t<label>name: </label>\n\t\t\t<div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n\t\t</div>\n\t\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,30 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Hero {
+	id: number;
+	name: string;
+}
 
 @Component({
 	selector: 'app',
-	template: '<h1>Dub Tee Ef Angular 2 App</h1>'
+	directives: [FORM_DIRECTIVES],
+	template: `
+		<h1>{{title}}</h1>
+		<h2>{{hero.name}} details!</h2>
+		<div><label>id: </label>{{hero.id}}</div>
+		<div>
+			<label>name: </label>
+			<div><input [(ng-model)]="hero.name" placeholder="name"></div>
+		</div>
+		`
 })
 
-class AppComponent {}
+class AppComponent {
+	public title = 'Tour of Heroes';
+	public hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}
+}
 
 bootstrap(AppComponent);
