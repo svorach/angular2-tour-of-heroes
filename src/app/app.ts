c7c5b269227edee4c549a8794dad260c08bcff43
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-10-26 14:20:16 -0400

    Added the angular2 starter.

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+	selector: 'app',
+	template: '<h1>Dub Tee Ef Angular 2 App</h1>'
+})
+
+class AppComponent {}
+
+bootstrap(AppComponent);
