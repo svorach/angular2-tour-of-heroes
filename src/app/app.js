3ca93477cddee940c200293ee93dc59a23b62f74
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-10-29 13:50:17 -0400

    Completed selection of hero.

--- src/app/app.js
+++ src/app/app.js
@@ -31,16 +31,17 @@ var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
         this.heroes = HEROES;
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
     }
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+    AppComponent.prototype.getSelectedClass = function (hero) {
+        return { 'selected': hero === this.selectedHero };
+    };
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'app',
-            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor],
-            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<ul class=\"heroes\">\n\t\t\t<li *ng-for=\"#hero of heroes\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}</a>\n\t\t\t</li>\n\t\t</ul>\n\t\t<div><label>id: </label>{{hero.id}}</div>\n\t\t<div>\n\t\t\t<label>name: </label>\n\t\t\t<div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n\t\t</div>\n\t\t"
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.CORE_DIRECTIVES],
+            styles: ["\n\t\t.heroes-container {\n\t\t\tdisplay: flex;\n\t\t}\n\t  .heroes {\n\t  \tlist-style-type: none;\n\t  \tmargin-left: 1em;\n\t  \tpadding: 0;\n\t  \twidth: 10em;\n\t  \tflex: 1;\n\t \t}\n\t  .heroes li {\n\t  \tcursor: pointer;\n\t  \tposition: relative;\n\t  \tleft: 0;\n\t  \ttransition: all 0.2s ease;\n\t    line-height: 1em;\n\t    padding: 5px 0;\n\t  }\n\t  .heroes li:hover,\n\t  .heroes li.selected {\n\t  \tcolor: #369;\n\t  \tbackground-color: #EEE;\n\t  \tleft: .2em;\n\t  \tpadding: 5px;\n\t  \tborder-radius: 5px;\n\t  }\n\t  .heroes .badge {\n\t    font-size: small;\n\t    color: white;\n\t    padding: 0.1em 0.7em;\n\t    background-color: #369;\n\t    position: relative;\n\t    border-radius: 3px;\n\t  }\n\t  .selected-hero {\n\t  \tflex: 3;\n\t  \tpadding: 0 20px;\n\t  \ttransition: all 1s ease;\n\t  \tanimation: 1s fadeIn;\n\t  }\n\n\t  @keyframes fadeIn {\n\t  \t0% {\n\t  \t\topacity: 0;\n\t  \t}\n\n\t  \t100% {\n\t  \t\topacity: 1;\n\t  \t}\n\t  }\n  "],
+            template: "\n\t\t<div class=\"heroes-container\">\n\t\t\t<ul class=\"heroes\">\n\t\t\t\t<li *ng-for=\"#hero of heroes\" (click)=\"onSelect(hero)\" [ng-class]=\"getSelectedClass(hero)\">\n\t\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}</a>\n\t\t\t\t</li>\n\t\t\t</ul>\n\t\t\t<div class=\"selected-hero\" *ng-if=\"selectedHero\">\n\t\t\t\t<h1>{{title}}</h1>\n\t\t\t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t\t\t<label>name: </label>\n\t\t\t\t<div><input [(ng-model)]=\"selectedHero.name\" placeholder=\"name\"></div>\n\t\t\t</div>\n\t\t</div>\n\t\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, bootstrap, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
 
 class Hero {
 	id: number;
@@ -20,19 +20,73 @@ var HEROES: Hero[] = [
 
 @Component({
 	selector: 'app',
-	directives: [FORM_DIRECTIVES, NgFor],
+	directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
+	styles:[`
+		.heroes-container {
+			display: flex;
+		}
+	  .heroes {
+	  	list-style-type: none;
+	  	margin-left: 1em;
+	  	padding: 0;
+	  	width: 10em;
+	  	flex: 1;
+	 	}
+	  .heroes li {
+	  	cursor: pointer;
+	  	position: relative;
+	  	left: 0;
+	  	transition: all 0.2s ease;
+	    line-height: 1em;
+	    padding: 5px 0;
+	  }
+	  .heroes li:hover,
+	  .heroes li.selected {
+	  	color: #369;
+	  	background-color: #EEE;
+	  	left: .2em;
+	  	padding: 5px;
+	  	border-radius: 5px;
+	  }
+	  .heroes .badge {
+	    font-size: small;
+	    color: white;
+	    padding: 0.1em 0.7em;
+	    background-color: #369;
+	    position: relative;
+	    border-radius: 3px;
+	  }
+	  .selected-hero {
+	  	flex: 3;
+	  	padding: 0 20px;
+	  	transition: all 1s ease;
+	  	animation: 1s fadeIn;
+	  }
+
+	  @keyframes fadeIn {
+	  	0% {
+	  		opacity: 0;
+	  	}
+
+	  	100% {
+	  		opacity: 1;
+	  	}
+	  }
+  `],
 	template: `
-		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-		<ul class="heroes">
-			<li *ng-for="#hero of heroes">
-				<span class="badge">{{hero.id}}</span> {{hero.name}}</a>
-			</li>
-		</ul>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>name: </label>
-			<div><input [(ng-model)]="hero.name" placeholder="name"></div>
+		<div class="heroes-container">
+			<ul class="heroes">
+				<li *ng-for="#hero of heroes" (click)="onSelect(hero)" [ng-class]="getSelectedClass(hero)">
+					<span class="badge">{{hero.id}}</span> {{hero.name}}</a>
+				</li>
+			</ul>
+			<div class="selected-hero" *ng-if="selectedHero">
+				<h1>{{title}}</h1>
+				<h2>{{selectedHero.name}} details!</h2>
+				<div><label>id: </label>{{selectedHero.id}}</div>
+				<label>name: </label>
+				<div><input [(ng-model)]="selectedHero.name" placeholder="name"></div>
+			</div>
 		</div>
 		`
 })
@@ -40,9 +94,12 @@ var HEROES: Hero[] = [
 class AppComponent {
 	public title = 'Tour of Heroes';
 	public heroes = HEROES;
-	public hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
+	public selectedHero: Hero;
+
+	onSelect(hero: Hero) { this.selectedHero = hero; }
+
+	getSelectedClass(hero: Hero) {
+		return { 'selected': hero === this.selectedHero };
 	}
 }
 
