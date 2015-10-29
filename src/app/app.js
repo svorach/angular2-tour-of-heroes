23aff1f017f456405797730886875357084a076a
Shane Vorachek <shane.vorachek@wolterskluwer.com>
2015-10-29 08:58:50 -0400

    Generating a list of heroes.

--- src/app/app.js
+++ src/app/app.js
@@ -15,9 +15,22 @@ var Hero = (function () {
     }
     return Hero;
 })();
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
         this.hero = {
             id: 1,
             name: 'Windstorm'
@@ -26,8 +39,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'app',
-            directives: [angular2_1.FORM_DIRECTIVES],
-            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<div><label>id: </label>{{hero.id}}</div>\n\t\t<div>\n\t\t\t<label>name: </label>\n\t\t\t<div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n\t\t</div>\n\t\t"
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor],
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<h2>{{hero.name}} details!</h2>\n\t\t<ul class=\"heroes\">\n\t\t\t<li *ng-for=\"#hero of heroes\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}</a>\n\t\t\t</li>\n\t\t</ul>\n\t\t<div><label>id: </label>{{hero.id}}</div>\n\t\t<div>\n\t\t\t<label>name: </label>\n\t\t\t<div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n\t\t</div>\n\t\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,34 @@
-import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
 
 class Hero {
 	id: number;
 	name: string;
 }
 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
 @Component({
 	selector: 'app',
-	directives: [FORM_DIRECTIVES],
+	directives: [FORM_DIRECTIVES, NgFor],
 	template: `
 		<h1>{{title}}</h1>
 		<h2>{{hero.name}} details!</h2>
+		<ul class="heroes">
+			<li *ng-for="#hero of heroes">
+				<span class="badge">{{hero.id}}</span> {{hero.name}}</a>
+			</li>
+		</ul>
 		<div><label>id: </label>{{hero.id}}</div>
 		<div>
 			<label>name: </label>
@@ -21,6 +39,7 @@ class Hero {
 
 class AppComponent {
 	public title = 'Tour of Heroes';
+	public heroes = HEROES;
 	public hero: Hero = {
 		id: 1,
 		name: 'Windstorm'
